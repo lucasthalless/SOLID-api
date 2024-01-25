9a508edf925473a6dd794286144a2b4e3f819a3e
lucas <lucaswtds@gmail.com>
2024-01-25 19:55:48 -0300

    feat: add send mail to createuser use case

--- src/useCases/CreateUser/CreateUserUseCase.ts
+++ src/useCases/CreateUser/CreateUserUseCase.ts
@@ -1,16 +1,20 @@
 import { User } from "../../entities/User";
+import { IMailProvider } from "../../providers/IMailProvider";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 import { ICreateUserRequestDTO } from "./CreateUserDTO";
 
 export class CreateUserUseCase {
 
   constructor (
-    private usersRepository: IUsersRepository
+    private usersRepository: IUsersRepository,
+    private mailProvider: IMailProvider
   ) {
 
   }
 
   async execute(data: ICreateUserRequestDTO) {
+    console.log(data);
+    
     const userAlreadyExists = await this.usersRepository.findByEmail(data.email);
 
     if(userAlreadyExists) {
@@ -19,6 +23,19 @@ export class CreateUserUseCase {
 
     const user = new User(data);
 
-    this.usersRepository.save(user);
+    await this.usersRepository.save(user);
+    
+    await this.mailProvider.sendMail({
+      to: {
+        name: data.name,
+        email: data.email
+      },
+      from: {
+        name: 'Aprendendo SOLID',
+        email: 'SOLIDapi@email.com'
+      },
+      subject: 'Seja bem vind@!',
+      body: '<p>Seu usuario foi criado com sucesso!</br>Você já pode fazer login na plataforma.</p>'
+    })
   }
 }
